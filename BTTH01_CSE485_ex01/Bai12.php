74651143418778d38668805f7c49f0bcb6004363
MinhKr <115930042+MinhKr@users.noreply.github.com>
2023-09-22 21:53:47 +0700

    up

---
+++ BTTH01_CSE485_ex01/Bai12.php
@@ -0,0 +1,43 @@
+<?php
+$numbers = [
+    'key1' => 12, 
+    'key2' => 30, 
+    'key3' => 4, 
+    'key4' => -123, 
+    'key5' => 1234, 
+    'key6' => -12565, 
+   ];
+
+//Lấy ra phần tử đầu tiên
+echo "Phần tử đầu tiên của mảng " . reset($numbers) . "<br>";
+
+//Lấy ra phần tử cuối cùng
+echo "Phần tử cuối cùng của mảng " . end($numbers) . "<br>";
+
+//Số lớn nhất
+$maxNumber = 0;
+foreach($numbers as $item){
+    if ($item > $maxNumber){
+        $maxNumber = $item ;
+    }
+}
+
+echo "Số lớn nhất trong mảng là " . $maxNumber ."<br>";
+
+//Số bé nhất
+$minNumber = PHP_INT_MAX;
+foreach($numbers as $item){
+    if ($item < $minNumber){
+        $minNumber = $item ;
+    }
+}
+
+echo "Số bé nhất trong mảng là " . $minNumber ."<br>";
+
+//Tổng
+$total = 0;
+foreach($numbers as $item){
+    $total = $total + $item;
+}
+
+echo "Tổng các phần tử trong mảng là " . $total ."<br>";
